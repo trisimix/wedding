08bacc2273f33fbda8e3589fa6f9e66fb6764b33
Josh Beaker <trisimix@protonmail.com>
2022-03-28 01:09:27 -0400

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Button from "./Button.svelte";
+  import Particles from "svelte-particles";
+
+  let particlesConfig = {
+    fpsLimit: 60,
+    particles: {
+      color: {
+        value: "#000"
+      },
+      links: {
+        enable: true,
+        color: "#000"
+      },
+      move: {
+        enable: true
+      }
+    }
+  };
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+
+  :global(#tsparticles) {
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+</style>
+
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Button />
+  <Particles options={particlesConfig} />
+</main>
